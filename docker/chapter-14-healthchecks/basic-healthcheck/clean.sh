850d9fe26a337070dca84d8df3aacef825b5a12a
Sandeep Dass <sandeepdass@Mac.ht.home>
2026-03-06 18:21:22 -0500

    [Docker] Chapter 14, basic-healthchecks, improvised the logging

--- docker/chapter-14-healthchecks/basic-healthcheck/clean.sh
+++ docker/chapter-14-healthchecks/basic-healthcheck/clean.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo "Stopping container..."
+
 docker stop health-basic 2>/dev/null || true
-docker rm -f health-basic 2>/dev/null || true
+
+echo "Removing container..."
+
+docker rm health-basic 2>/dev/null || true
+
+echo "Removing image..."
 
 docker rmi healthcheck-basic 2>/dev/null || true
+
+echo "Cleanup complete."
--- docker/chapter-14-healthchecks/basic-healthcheck/run.sh
+++ docker/chapter-14-healthchecks/basic-healthcheck/run.sh
@@ -2,17 +2,22 @@
 
 docker build -t healthcheck-basic .
 
-docker run --rm -d \
+docker run -d \
   -p 5001:5001 \
   --name health-basic \
   healthcheck-basic
 
 echo ""
-echo "Container started..."
-echo ""
+echo "======================================"
+echo "Container started"
+echo "======================================"
 
 echo "Waiting for 15 seconds so that the container can be started properly..."
 
 sleep 15
+
+echo ""
+echo "Container Status:"
+echo ""
 
 docker ps
